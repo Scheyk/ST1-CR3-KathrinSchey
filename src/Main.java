83fa5871c553d2a48178d7eb37ce5f8fbd8d8b9b
Scheyk <kathrin.dostal@gmail.com>
2020-10-23 10:25:52 +0200

    finish with "normal part" and the first bonus part. lets start with the last one :)

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,52 @@
+import java.lang.reflect.Array;
+
 public class Main {
 
     public static void main(String[]args){
+
+        TheMethods rectangular = new TheMethods();
+        rectangular.rectangular();
+
+        TheMethods calculateRectArea = new TheMethods();
+        System.out.println("The rectangular  Area is " + calculateRectArea.calculateRectArea(20,30) + "qm.");
+        System.out.println("The rectangular  Area is " + calculateRectArea.calculateRectArea(40,50) + "qm.");
+        System.out.println(" ");
+
+        TheMethods squareArea = new TheMethods();
+        System.out.println("The square Area is " + squareArea.squareArea(4) + "qm.");
+        System.out.println("The square Area is " + squareArea.squareArea(2) + "qm.");
+        System.out.println(" ");
+
+        TheMethods one = new TheMethods();
+        System.out.println("The rectangular Area is " + one.calculateRectArea(5,8) + "qm.");
+        System.out.println(" ");
+
+
+        TheMethods two = new TheMethods();
+        System.out.println("The rectangular Area is " + two.calculateRectArea(2,7) + "qm.");
+        System.out.println(" ");
+
+        TheMethods three = new TheMethods();
+        System.out.println("The rectangular Area is " + three.calculateRectArea(60,79) + "qm.");
+        System.out.println(" ");
+
+        TheMethods four = new TheMethods();
+        System.out.println("The square Area is " + four.squareArea(77) + "qm.");
+        System.out.println(" ");
+
+        TheMethods five = new TheMethods();
+        System.out.println("The square Area is " + five.squareArea(8) + "qm.");
+        System.out.println(" ");
+
+        TheMethods six = new TheMethods();
+        System.out.println("The square Area is " + six.squareArea(35) + "qm.");
+        System.out.println(" ");
+
+        int[] array = {one.calculateRectArea(8,89), two.calculateRectArea(22,77), three.calculateRectArea(64,8)};
+
+        for(int elements : array){
+            System.out.println("result: " + elements);
+        }
 
     }
 }
--- src/TheMethods.java
+++ src/TheMethods.java
@@ -1,2 +1,27 @@
+import java.util.Scanner;
+
 public class TheMethods {
+
+    static void rectangular(){
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("enter the length");
+        double l = in.nextDouble();
+        System.out.println("enter the width");
+        double w = in.nextDouble();
+
+        double result = l*w;
+        System.out.println("The area of this rectangular is: " + result);
+        System.out.println(" ");
+    }
+
+    static int calculateRectArea(int a, int b){
+        return a*b;
+    }
+
+    static int squareArea(int a){
+        return a*4;
+    }
+
+
 }
