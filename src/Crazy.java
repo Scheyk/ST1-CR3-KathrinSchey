26465f1b401c59f5b4e13056d48f95370b77d901
Scheyk <kathrin.dostal@gmail.com>
2020-10-24 16:46:30 +0200

    chnage the name of teh variables on line 21 and 22. also i make a system.out before the scanner start.

--- src/Crazy.java
+++ src/Crazy.java
@@ -10,15 +10,17 @@ public class Crazy {
 
     public static void main(String[]args){
 
+        System.out.println("enter two numbers to know the qm");
+
         for (int i = 0; i < args.length; i++){  // <---- solution with scanner to pass the arguments
             Scanner in = new Scanner(System.in);
             System.out.println("enter number");
             args[i] += in.nextInt();
         }
 
-        int one = Integer.parseInt(args[0]);  // <--- get new argument 1
-        int two = Integer.parseInt(args[1]);  // <--- get new argument 2
-        int result2 = one*two;  // <--- result from argument 1 and two
+        int width = Integer.parseInt(args[0]);  // <--- get new argument 1
+        int height = Integer.parseInt(args[1]);  // <--- get new argument 2
+        int result2 = width*height;  // <--- result from argument 1 and two
 
         System.out.println("your result is: " + result2);
     }
